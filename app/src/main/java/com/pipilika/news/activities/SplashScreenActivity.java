e823da3e18687c34bb390492301b33851c5f2d9d
RaggedyCoder <bluepandora666@gmail.com>
2015-05-06 23:40:43 +0600

    code reformatted comment needed

--- app/src/main/java/com/pipilika/news/activities/SplashScreenActivity.java
+++ app/src/main/java/com/pipilika/news/activities/SplashScreenActivity.java
@@ -6,18 +6,13 @@ import android.support.v7.app.ActionBarActivity;
 import com.pipilika.news.R;
 import com.pipilika.news.fragments.SplashScreenFragment;
 
-/**
- * Created by tuman on 3/5/2015.
- */
 public class SplashScreenActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new SplashScreenFragment())
-                    .commit();
+            getSupportFragmentManager().beginTransaction().add(R.id.container, new SplashScreenFragment()).commit();
         }
     }
 }
