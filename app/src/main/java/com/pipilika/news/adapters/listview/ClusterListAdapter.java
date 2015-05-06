09ce7d0cdf82b4b0017c1a4192fe31f1501abca4
RaggedyCoder <bluepandora666@gmail.com>
2015-05-06 23:40:55 +0600

    code reformatted comment needed

--- app/src/main/java/com/pipilika/news/adapters/listview/ClusterListAdapter.java
+++ app/src/main/java/com/pipilika/news/adapters/listview/ClusterListAdapter.java
@@ -2,7 +2,6 @@ package com.pipilika.news.adapters.listview;
 
 import android.app.Activity;
 import android.content.Context;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,21 +17,19 @@ import com.pipilika.news.view.widget.CustomViewPager;
 
 import java.util.List;
 
-/**
- * Created by tuman on 21/4/2015.
- */
 public class ClusterListAdapter extends BaseAdapter implements Response.ErrorListener {
 
     private List<ClusterListItem> clusterListItems;
     private ClusterPagerAdapter clusterPagerAdapter;
     private LayoutInflater inflater;
     private Activity activity;
+    private String zipId;
 
-    public ClusterListAdapter(Activity activity, List<ClusterListItem> clusterListItems) {
+    public ClusterListAdapter(Activity activity, List<ClusterListItem> clusterListItems, String zipId) {
         this.activity = activity;
         this.clusterListItems = clusterListItems;
-        Log.e("TAG SIZE", clusterListItems.size() + "size");
         inflater = (LayoutInflater) this.activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.zipId = zipId;
     }
 
     @Override
@@ -58,19 +55,16 @@ public class ClusterListAdapter extends BaseAdapter implements Response.ErrorLis
             holder = new ViewHolder();
             holder.clusterPager = (CustomViewPager) convertView.findViewById(R.id.cluster_pager);
             holder.clusterPager.setClipToPadding(false);
-            holder.clusterPager.setPageMargin((int) TypedValue.
-                    applyDimension(TypedValue.COMPLEX_UNIT_DIP, 12, activity.getResources().getDisplayMetrics()));
-
+            holder.clusterPager.setPageMargin((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 12, activity.getResources().getDisplayMetrics()));
             convertView.setTag(holder);
         } else {
             holder = (ViewHolder) convertView.getTag();
         }
-        Log.e("TAG", position + " of list");
         holder.clusterPager.setPageMargin(15);
         holder.clusterPager.setPageMargin(15);
         holder.clusterPager.setClipChildren(false);
-        clusterPagerAdapter = new ClusterPagerAdapter(activity, clusterListItems.get(position).getNews(),
-                clusterListItems.get(position).getCategory());
+        ClusterListItem clusterListItem = clusterListItems.get(position);
+        clusterPagerAdapter = new ClusterPagerAdapter(activity, clusterListItem.getNews(), clusterListItem.getCategory(), zipId);
         holder.clusterPager.setAdapter(clusterPagerAdapter);
         return convertView;
     }
